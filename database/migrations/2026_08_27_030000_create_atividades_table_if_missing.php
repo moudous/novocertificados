e35fa72eced48b88b441f011e3badd55bddc005d
Marcelo Ribeiro da Silva <marcelo.ribeiro@montesclaros.mg.gov.br>
2026-08-28 10:28:25 -0300

    remoção de template_id de atividades

--- database/migrations/2026_08_27_030000_create_atividades_table_if_missing.php
+++ database/migrations/2026_08_27_030000_create_atividades_table_if_missing.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->smallInteger('ativo')->nullable();
             $table->string('imagemFundo', 50)->nullable();
             $table->text('template_php')->nullable();
-            $table->integer('template_id')->nullable();
             $table->dateTime('criado_em')->nullable()->useCurrent();
             $table->dateTime('atualizado_em')->nullable()->useCurrent();
             $table->dateTime('apagado_em')->nullable()->index();
